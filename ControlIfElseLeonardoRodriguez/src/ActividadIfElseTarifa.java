a38dc0f98fdad8e5050c599b49f02ad4b7f6f61c
Leonardo Rodriguez <20253DS137@utez.edu.mx>
2026-01-22 10:06:21 -0600

    base fix error

--- ControlIfElseLeonardoRodriguez/src/ActividadIfElseTarifa.java
+++ ControlIfElseLeonardoRodriguez/src/ActividadIfElseTarifa.java
@@ -5,60 +5,72 @@ public class ActividadIfElseTarifa {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int edad;
+        int tarifa = 0;
         String respuesta=" ";
         boolean esEstudiante;
 
         edad=obtenerEdad(scanner,"Ingrese su edad");
         if (edad>=18){
-            System.out.println("¿Es estudiante?");
+            System.out.println("Eres estudiante?");
             scanner.nextLine();
             respuesta=obtenerEstudiante(scanner,"Si o No");
             if (respuesta.equals("Si")|| respuesta.equals("si")){
                 esEstudiante=true;
-
-                System.out.println("La edad ingresada es: " + edad);
-                System.out.println("La persona es estudiante");
-                System.out.println("La tarifa final es: 90 ");
+                tarifa = 90;
             }else if(respuesta.equals("No")|| respuesta.equals("no")){
                 esEstudiante=false;
-
-                System.out.println("La edad ingresada es: " + edad);
-                System.out.println("La persona no es estudiante");
-                System.out.println("La tarifa final es: 120 ");
+                tarifa = 120;
             }
         } else if (edad>12 && edad<=17){
-            System.out.println("¿Es estudiante?");
+            System.out.println("Eres estudiante?");
             scanner.nextLine();
             respuesta=obtenerEstudiante(scanner,"Si o No");
             if (respuesta.equals("Si")|| respuesta.equals("si")){
                 esEstudiante=true;
-
-                System.out.println("La edad ingresada es: " + edad);
-                System.out.println("La persona es estudiante");
-                System.out.println("La tarifa final es: 60 ");
+                tarifa = 60;
             }else if(respuesta.equals("No")|| respuesta.equals("no")){
                 esEstudiante=false;
-
-                System.out.println("La edad ingresada es: " + edad);
-                System.out.println("La persona no es estudiante");
-                System.out.println("La tarifa final es: 80 ");
+                tarifa = 80;
             }
         } else if (edad<12) {
-            System.out.println("La edad ingresada es: " + edad);
-            System.out.println("La tarifa final es: 50 ");
+            System.out.println("Eres estudiante?");
+            scanner.nextLine();
+            respuesta=obtenerEstudiante(scanner,"Si o No");
+            if (respuesta.equals("Si")|| respuesta.equals("si")){
+                esEstudiante=true;
+                tarifa = 50;
+            } else if (respuesta.equals("Si")|| respuesta.equals("si")){
+                esEstudiante=false;
+                tarifa = 50;
+            }
         }
         if (edad <0 || edad> 120) {
             System.out.println("Edad invalida");
+        } else {
+            System.out.println("La edad ingresada es: " + edad);
+            System.out.println("La persona es estudiante");
+            System.out.println("La tarifa final es: " + tarifa);
         }
 
     }
 
+    /** Metodo que retorna un valor capturado por la terminal
+     *
+     * @param scanner -> previamente declarado en main
+     * @param mensaje -> es un parametro
+     * @return -> int
+     */
     public static int obtenerEdad(Scanner scanner, String mensaje){
         System.out.println(mensaje);
         return scanner.nextInt();
     }
 
-
+    /** Metodo que retorna un valor capturado por la terminal y un mensaje previamente declarado
+     *
+     * @param scanner -> previamente declarado en main
+     * @param mensaje -> es un parametro
+     * @return -> String de mensaje
+     */
     public static String obtenerEstudiante(Scanner scanner, String mensaje){
         System.out.println(mensaje);
         return scanner.nextLine();
