e457e5b6ed113f539766210c2825e56e255fc965
David-Jimenez-Gon2604 <20253rd001@utez.edu.mx>
2026-02-05 09:35:11 -0600

    base ControlDeCalificaciones

---
+++ ControlDeCalificaciones/src/GradeService.java
@@ -0,0 +1,8 @@
+public class GradeService {
+
+    private double a = 0;
+    private double b = 0;
+    private double c = 0;
+
+
+}
---
+++ ControlDeCalificaciones/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+        public static void main(String[] args) throws Exception {
+            GradeService gradeService = new GradeService();
+            Scanner sc = new Scanner(System.in);
+
+            public String leerTextoNoVacio(Scanner sc, String msg){
+                return=0;
+            }
+
+            public double leerDoubleEnRango(Scanner sc, String msg, double min, double max){
+                return=0;
+            }
+
+            public int leerIntEnRango(Scanner sc, String msg, int min, int max){
+                return=0;
+            }
+
+            public boolean leerBoolean(Scanner sc, String msg){
+                return=0;
+            }
+
+
+        }
+
+}
